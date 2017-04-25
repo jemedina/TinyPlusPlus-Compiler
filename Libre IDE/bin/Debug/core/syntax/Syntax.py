690bc85452d0dc03629d73291e55a1a956dbb3ac
Eduardo <jemedina.96@gmail.com>
2017-04-24 20:35:48 -0500

    Improving the Token Helper class

--- Libre IDE/bin/Debug/core/syntax/Syntax.py
+++ Libre IDE/bin/Debug/core/syntax/Syntax.py
@@ -2,7 +2,7 @@ from Tree import *
 from TokensHelper import *
 
 def main():
-	tokensHelper.match()
+	tokensHelper.match("identifier")
 #programa → main “{“ lista-declaración lista-sentencias “}”
 def programa():
 	print("Unimplemented prorgama method")
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 		if sys.argv[1] == "-t":
 			print("#### Tests for Syntax.py ####")
 		else:
-			tokensHelper = TokensHelper()
+			tokensHelper = TokensHelper(sys.argv[1])
 			main()
 	else:
 		print("Invalid call to Syntax program... plase use:\n[python] Syntax.py tokensFile.out",file=sys.stderr)
--- Libre IDE/bin/Debug/core/syntax/TokensHelper.py
+++ Libre IDE/bin/Debug/core/syntax/TokensHelper.py
@@ -1,5 +1,40 @@
+from os import sys
+class Token:
+	def __init__(self,type,content):
+		self.type = type
+		self.content = content
+
+	def __str__(self):
+		return "{"+self.identifier+":"+self.content+"}"
+
 class TokensHelper:
-	def loadTokens(self,tokensFilePath):
-		print("Unimplemented loadTokens method")
-	def match(self):
-		print("Unimplemented match method")
+
+	def __init__(self,tokensFilePath):
+		try:
+			self.tokensFile = open(tokensFilePath,"+r")
+			self.tokens = []
+			self.index = -1
+			self.loadTokens()
+		except Exception as e:
+			print("File ["+tokensFilePath+"] was not found...",file=sys.stderr)
+			self.tokensFile = None
+			sys.exit(1)
+
+	def loadTokens(self):
+		for tokenText in self.tokensFile:
+			if tokenText.find(":") > 0:
+				identifier = tokenText[0:tokenText.find(":")]
+				content = tokenText[tokenText.find(":")+1:len(tokenText)-1]
+				self.tokens.append(Token(identifier,content))
+
+	def match(self,tokenType):
+		if tokenType == self.getCurrentToken().type:
+			return getToken()
+		else:
+			return None
+	def getToken(self):
+		self.index += 1
+		return self.tokens[self.index]
+
+	def getCurrentToken(self):
+		return self.tokens[self.index]
