5d525ad69865ee244645721cf462ea4a6e11ab7c
Eduardo <jemedina.96@gmail.com>
2017-04-30 10:47:22 -0500

    if statement added

--- Libre IDE/bin/Debug/core/syntax/Syntax.py
+++ Libre IDE/bin/Debug/core/syntax/Syntax.py
@@ -1,8 +1,11 @@
 from Tree import *
 from TokensHelper import *
 ################### ENDPOINTS
-tipo = ["int","float","boolean"]
-sentencia = ["if","while","do","cin","cout","{"]
+_tipo = ["int","float","boolean"]
+_sentencia = ["if","while","do","cin","cout","{"]
+_relacion = ["<=", "<", ">" ,">=", "==", "!="]
+_suma_op = ["+","-"]
+_mult_op = ["*","/"]
 #############################
 
 
@@ -24,7 +27,7 @@ def programa():
 #lista-declaración -> { declaración; }
 def lista_declaracion():
 	tmp = Node("lista-declaracion")
-	while tokensHelper.getCurrentToken().content in tipo:
+	while tokensHelper.getCurrentToken().content in _tipo:
 		tmp.addChild(declaracion())
 		tokensHelper.match(";")
 	if len(tmp.sons) > 0:
@@ -36,7 +39,7 @@ def lista_declaracion():
 #sentencia → selección | iteración | repetición | sent-cin |sent-out | bloque | asignación
 def lista_sentencias():
 	tmp = Node("lista-sentencias")
-	while tokensHelper.getCurrentToken().content in sentencia or tokensHelper.getCurrentToken().type == TokenConstants.ID:
+	while tokensHelper.getCurrentToken().content in _sentencia or tokensHelper.getCurrentToken().type == TokenConstants.ID:
 		if tokensHelper.getCurrentToken().content == TokenConstants.IF:
 			tmp.addChild( seleccion() )
 		elif tokensHelper.getCurrentToken().content == TokenConstants.WHILE:
@@ -54,9 +57,90 @@ def lista_sentencias():
 		return tmp
 	else:
 		return None
-
+#selección → if ( expresión ) then bloque | if ( expresión ) then bloque else bloque
 def seleccion():
-	return None
+	ifStmt = Node( TokenConstants.IF )
+	tokensHelper.match( TokenConstants.IF )
+	tokensHelper.match( "(" )
+	ifStmt.addChild ( expresion() )
+	tokensHelper.match( ")" )
+	tokensHelper.match( "then" )
+	ifStmt.addChild( bloque() )
+
+	if( tokensHelper.getCurrentToken().content == TokenConstants.ELSE ):
+		tokensHelper.match( TokenConstants.ELSE )
+		ifStmt.addChild( bloque() )
+	
+	
+	return ifStmt
+	
+#expresión → expresión-simple relación expresión-simple | expresión-simple
+def expresion():
+	exp = Node("expresion")
+	exp.addChild( expresion_simple() )
+	if( tokensHelper.getCurrentToken().content in _relacion ):
+		exp.addChild( relacion() )
+		exp.addChild( expresion_simple() )
+	return exp
+
+def relacion():
+	rel = tokensHelper.getCurrentToken().content
+	tokensHelper.match(TokenConstants.RELATION,True)
+	return Node(rel)
+
+#expresión-simple → expresión-simple suma-op termino | termino
+#expresión-simple → termino { suma-op termino }
+def expresion_simple():
+	exp_simp = Node("expresion-simple")
+	exp_simp.addChild( termino() )
+	while( tokensHelper.getCurrentToken().content in _suma_op):
+		exp_simp.addChild( suma_op() )
+		exp_simp.addChild( termino() )
+	return exp_simp
+
+def suma_op():
+	rel = tokensHelper.getCurrentToken().content
+	if(tokensHelper.getCurrentToken().type == TokenConstants.PLUS):
+		tokensHelper.match(TokenConstants.PLUS,True)
+	elif (tokensHelper.getCurrentToken().type == TokenConstants.LESS):
+		tokensHelper.match(TokenConstants.LESS,True)
+	return Node(rel)
+
+#termino → termino mult-op factor | factor
+#termino → factor { mult-op factor }
+def termino():
+	term = Node("termino")
+	term.addChild( factor() )
+	while ( tokensHelper.getCurrentToken().content in _mult_op):
+		term.addChild( mult_op() )
+		term.addChild( factor() )
+	return term
+
+def mult_op():
+	rel = tokensHelper.getCurrentToken().content
+	if(tokensHelper.getCurrentToken().content == TokenConstants.TIMES):
+		tokensHelper.match(TokenConstants.TIMES)
+	elif (tokensHelper.getCurrentToken().content == TokenConstants.DIV):
+		tokensHelper.match(TokenConstants.DIV)
+	return Node(rel)
+
+#factor → ( expresión ) | numero | identificador
+def factor():
+	new = None
+	if tokensHelper.getCurrentToken().content == "(":
+		tokensHelper.match("(")
+		new = expresion()
+		tokensHelper.match(")")
+	
+	elif tokensHelper.getCurrentToken().type == TokenConstants.INT:
+		new = Node(tokensHelper.getCurrentToken().content)
+		tokensHelper.match(TokenConstants.INT,True)
+
+	elif tokensHelper.getCurrentToken().type == TokenConstants.ID:
+		new = Node(tokensHelper.getCurrentToken().content)
+		tokensHelper.match(TokenConstants.ID,True)
+
+	return new
 
 def iteracion():
 	return None
