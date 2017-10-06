e8e1b2fcd398d9a14533be69c2bcb4f0ee92dd9c
Eduardo <jemedina.96@gmail.com>
2017-10-05 19:59:37 -0500

    Tabla de simbolos

--- tinypp/semantic/Semantic.py
+++ tinypp/semantic/Semantic.py
@@ -1,19 +1,27 @@
 from semantic.HashTable import *
+from semantic.DictionaryUtility import *
 import json
 import os
 import ntpath
 import platform
 
+
 class Semantic:
 
 	def __init__(self, path):
+		#LOAD TREE
 		jsonFile = self.openSyntaxTree(path)
+		self.tree = DictionaryUtility.to_object(json.load(jsonFile))
+
+		### TEST HASH TABLE ###
 		hashTable = HashTable()
 		hashTable.add('x',1,None,'int')
 		hashTable.add('y',3,None,'int')
 		hashTable.add('y',5,None,'int')
 		hashTable.cliDisplayTable()
-	
+	def getTree(self):
+		return self.tree
+
 	def openSyntaxTree(self, pathOfSouce):
 		canonicalFileName = ntpath.basename(pathOfSouce)
 		withoutExtention = canonicalFileName[0:canonicalFileName.find(".")]
@@ -21,14 +29,14 @@ class Semantic:
 		basepath = ntpath.abspath(pathOfSouce)[0:len(ntpath.abspath(pathOfSouce))-len(canonicalFileName)]
 		if  platform.system() != 'Linux':
 			try: 
-				jsonFile = open(basepath+synDirectory+"out.json","+w",encoding='utf-8')
+				jsonFile = open(basepath+synDirectory+"out.json","r",encoding='utf-8')
 				return jsonFile
 			except OSError:
 				pass
 		else:
 			synDirectory = "target_"+withoutExtention+"/syn/"
 			try: 
-				jsonFile = open(synDirectory+"out.json","+w",encoding='utf-8')
+				jsonFile = open(synDirectory+"out.json","r",encoding='utf-8')
 				return jsonFile
 			except OSError:
 				pass
